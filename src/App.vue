1bcea73cf28c9d3681ae018a125301f4c3e0af48
hashrock <hashrock@users.noreply.github.com>
2017-11-06 03:09:47 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <cute-button></cute-button>
+  </div>
+</template>
+
+<script>
+import CuteButton from "./CuteButton.vue"
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  components: {
+    CuteButton
+  }
+}
+</script>
+
+<style>
+body{
+  background: #EFE;
+  margin-top: 5rem;
+  text-align: center;
+}
+
+</style>
---
+++ src/CuteButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <svg width="400" height="80" @mousemove="over">
+    <rect width="200" height="50" rx="8" ry="8" x="20" y="20">
+    </rect>
+    <text x="120" y="54" text-anchor="middle">I am Button!</text>
+    <circle r="10" cx="100" cy="20" class="eye-outer"></circle>
+    <circle r="10" cx="130" cy="20" class="eye-outer"></circle>
+    <circle r="5" :cx="100 + dx" :cy="20 + dy" class="eye-inner"></circle>
+    <circle r="5" :cx="130 + dx" :cy="20 + dy" class="eye-inner"></circle>
+  </svg>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      dx: 2,
+      dy: 2
+    }
+  },
+  methods: {
+    over(ev){
+      const cx = 100
+      const cy = 20
+      const dx = ev.offsetX - cx
+      const dy = ev.offsetY - cy
+      this.dx = dx / 400 * 4
+      this.dy = dy / 80 * 4
+      console.log(dx, dy )
+    }
+  }
+}
+</script>
+<style scoped>
+rect{
+  fill: white;
+  stroke: #793;
+  stroke-width: 5px;
+  cursor: pointer;
+}
+rect:hover{
+  fill: #EFE;
+}
+
+.eye-outer{
+  fill: white;
+  stroke: #793;
+  stroke-width: 5px;
+}
+text{
+  cursor: pointer;
+  pointer-events: none;
+}
+
+.eye-inner{
+  fill: #793;
+}
+</style>
